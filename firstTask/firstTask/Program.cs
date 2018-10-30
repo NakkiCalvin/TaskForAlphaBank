d01019a87eb0540931cb9cc16be51a364c50acc4
nakkicalvin <nakkicalvin@gmail.com>
2018-10-30 16:24:56 +0300

    ver 1.0

---
+++ firstTask/firstTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int[] array = new int[] { 2, 4, 1, 3 };
+
+
+            var arrSorted = (from item in array
+                            orderby item descending
+                            select item).ToArray();
+
+
+            int result = Bsearch(arrSorted, x);
+            
+            Console.WriteLine(result);
+        }
+
+        public static int Bsearch(int[] array, int X)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] < X)
+                {
+                    return index;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return 0;
+        }
+    }
+}
